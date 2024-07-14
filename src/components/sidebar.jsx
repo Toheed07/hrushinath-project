5681aaccdaa7abb389e90cec1c1d088454899716
toheed-gravitii <toheed@gravitii.xyz>
2024-07-14 19:46:15 +0530

    completed the project

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,38 +1,43 @@
 import { useState } from "react";
 
+import { useMediaQuery } from 'react-responsive';
+
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
+
+  const isDesktopOrLaptop = useMediaQuery({
+    query: '(min-device-width: 768px)'
+  });
 
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
   };
+  
 
   return (
+    <>
+
     <div className="flex">
-     
-      {/* <button
-        data-drawer-target="default-sidebar"
-        data-drawer-toggle="default-sidebar"
-        aria-controls="default-sidebar"
-        type="button"
-        className="inline-flex items-center p-1 mt-2 ml-3 text-sm text-gray-500 rounded-lg  hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-        onClick={toggleSidebar}
-      >
-        <span className="sr-only">Open sidebar</span>
-        <svg
-          className="w-6 h-6"
-          aria-hidden="true"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
+    {!isDesktopOrLaptop && (
+        <button
+          className="w-12 h-12 bg-gray-200 absolute top-4 right-4 flex items-center justify-center rounded-full"
+          onClick={toggleSidebar}
         >
-          <path
-            clipRule="evenodd"
-            fillRule="evenodd"
-            d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
-          ></path>
-        </svg>
-      </button> */}
+          <svg
+            className="w-6 h-6 text-gray-600"
+            aria-hidden="true"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              clipRule="evenodd"
+              fillRule="evenodd"
+              d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+            ></path>
+          </svg>
+        </button>
+      )}
 
       <aside
         id="default-sidebar"
@@ -42,7 +47,7 @@ const Sidebar = () => {
         aria-label="Sidebar"
       >
         <div className="h-full px-3  py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-          {isOpen && (
+    {isOpen && (
             <button className="w-full top-0 left-0" onClick={toggleSidebar}>
               <svg
                 className="w-6 h-6"
@@ -222,7 +227,7 @@ const Sidebar = () => {
         </div>
       </aside>
     </div>
+    </>
   );
 };
-
-export default Sidebar;
+ export default Sidebar
